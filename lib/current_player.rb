a0e657bd417be9e98999fe482d01cdd644771bee
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-07 00:35:15 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   number_of_turns = 0 
   board.each do |spaces|
-    if spaces === "X" || spaces === "O"
+    if spaces == "X" || spaces == "O"
     counter += 1
     end
 end 
